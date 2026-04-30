4042e1e2297e5e5c24474e25a8c2af91c4689c2e
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2026-04-29 19:45:41 -0600

    colas de listos funcionales
    Falta implementar los schedulers

--- Proyecto1/SchedulingShips_IDF/main/ship.c
+++ Proyecto1/SchedulingShips_IDF/main/ship.c
@@ -1,43 +1,64 @@
 #include "ship.h"
 #include <stdio.h>
 
-// Función para inicializar un barco con los valores dados
-void inicializar_barco(Ship* barco, int id, ShipType type) {
+/*
+ * Inicializa una estructura Ship con los datos recibidos.
+ *
+ * Parámetros:
+ * - barco: puntero a la estructura Ship que será inicializada.
+ * - id: identificador único del barco.
+ * - type: tipo de barco.
+ * - dir: dirección del barco.
+ *
+ * La velocidad se asigna dependiendo del tipo de barco.
+ */
+void inicializar_barco(Ship* barco, int id, ShipType type, Direction dir) {
     barco->id = id;
     barco->type = type;
+    barco->direction = dir;
 
     switch (type) {
-    case NORMAL:
-        barco->speed = 1.0;
-        break;
-    case PESQUERO:
-        barco->speed = 2.0;
-        break;
-    case PATRULLA:
-        barco->speed = 3.0;
-        break;
-    default:
-        barco->speed = 1.0;
+        case NORMAL:
+            barco->speed = 1.0;
+            break;
+        case PESQUERO:
+            barco->speed = 2.0;
+            break;
+        case PATRULLA:
+            barco->speed = 3.0;
+            break;
+        default:
+            barco->speed = 1.0;
         break;
     }
 }
 
+/*
+ * Devuelve una abreviatura del tipo de barco.
+ *
+ * N  = Normal
+ * PE = Pesquero
+ * PA = Patrulla
+ */
+const char* shipTypeToString(ShipType type) {
+    switch(type) {
+    case NORMAL: return "N";
+    case PESQUERO: return "PE";
+    case PATRULLA: return "PA";
+    default: return "?";
+    }
+}
 
-void printShip(const Ship* b) {
-    printf("Tipo de barco: ");
-
-    switch(b->type) {
-        case NORMAL:
-            printf("Normal");
-            break;
-        case PESQUERO:
-            printf("Pesquero");
-            break;
-        case PATRULLA:
-            printf("Patrulla");
-            break;
-        default:
-            printf("Desconocido");
-            break;
+/*
+ * Devuelve una abreviatura de la dirección del barco.
+ *
+ * IZQ = Izquierda
+ * DER = Derecha
+ */
+const char* dirToString(Direction dir) {
+    switch(dir) {
+    case IZQUIERDA: return "IZQ";
+    case DERECHA: return "DER";
+    default: return "?";
     }
 }
